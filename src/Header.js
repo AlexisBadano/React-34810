2a1abb2001b1965ce5a37a8b6b630248ff60dca3
Alexis Badano <Alexis.badano@gmail.com>
2022-10-27 21:07:23 -0300

    creando componentes

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <header>
+            <navbar>
+                <ul>
+                   <li>Home</li> 
+                   <li>Tienda</li> 
+                   <li>Contacto</li> 
+                   <li></li> 
+                </ul>
+            </navbar>
+        </header>
+    )
+}
+
+export default Header
