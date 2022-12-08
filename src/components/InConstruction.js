0e93b4a92f6e0db1169035cee5d4eec94c4ce6ff
Alexis Badano <Alexis.badano@gmail.com>
2022-12-08 12:05:10 -0300

    Updates

--- src/components/InConstruction.js
+++
@@ -1,7 +0,0 @@
-const InContruction = () => {
-    return (
-        <h1>Sorry, this section is currently under construction and therefore unavailable </h1>
-    )
-}
-
-export default InContruction;
